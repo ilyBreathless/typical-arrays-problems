b16e1ff10d283e307e9008466e298c03e3a688ad
ilyBreathless <72036113+ilyBreathless@users.noreply.github.com>
2021-03-11 15:11:58 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 
 exports.min = function min (array) {
+    if (typeof array === 'undefined' || !array.length) {
+        return 0;
+    }
+    return Math.min.apply(null, array);
   return 0;
 }
 
 exports.max = function max (array) {
+    if (typeof array === 'undefined' || !array.length) {
+        return 0;
+    }
+    return Math.max.apply(null, array);
   return 0;
 }
 
 exports.avg = function avg (array) {
+    if (typeof array === 'undefined' || !array.length) {
+        return 0;
+    }
+    let sum = 0;
+    for(let i = 0; i < array.length; i++) {
+        sum += array[i];
+    }
+    let avg = sum / array.length;
+    return avg;
   return 0;
 }
